3f74957f1aae541fd75d63a02e7cf99e5b38651a
BroaderImpact <BroaderImpact@users.noreply.github.com>
2022-01-01 21:52:24 -0500

    update demo.R
    Signed-off-by: BroaderImpact <BroaderImpact@users.noreply.github.com>

--- demo.R
+++ demo.R
@@ -11,9 +11,10 @@ circleFun <- function(center = c(0, 0), diameter = 1, npoints = 100){
     yy <- center[2] + r * sin(tt)
     return(data.frame(x = xx, y = yy))
 }
-
+print(circleFun)
 dat <- 
   circleFun(c(1, -1), 2.3, npoints = 100)
 
 ggplot(dat,aes(x, y)) +
   geom_path()
+print(dat)
